456bae40c8a4f4448c31445b08b51a10b45c3424
Mateus Lima <mateuslima020@outlook.com>
2024-04-11 00:28:32 -0300

    add authentication

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { JwtService } from '@nestjs/jwt'
+
+// const createAccountBodySchema = z.object({
+//  name: z.string(),
+//  email: z.string().email(),
+//  password: z.string(),
+// })
+
+// type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(private jwt: JwtService) {}
+
+  @Post()
+  // @HttpCode(201)
+  // @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle() {
+    const token = this.jwt.sign({
+      sub: 'user-id',
+    })
+
+    return token
+  }
+}
