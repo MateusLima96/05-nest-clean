e1ef0821b4f9ae7ebfff616bf133b442ff099d55
Mateus de Araújo Lima <mateuslima020@outlook.com>
2024-05-03 22:54:17 -0300

    Add Best Question Answer Controller

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -4,6 +4,7 @@ import { Question } from '../../enterprise/entities/question'
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from '../../../../core/errors/errors/resource-not-found-error.error'
 import { NotAllowedError } from '../../../../core/errors/errors/not-allowed-error'
+import { Injectable } from '@nestjs/common'
 
 interface ChooseQuestionBestAnswerUseCaseRequest {
   answerId: string
@@ -16,6 +17,8 @@ type ChooseQuestionBestAnswerUseCaseResponse = Either<
     question: Question
   }
 >
+
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase {
   constructor(
     private answersRepository: AnswersRepository,
